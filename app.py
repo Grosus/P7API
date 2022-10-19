a05983f5ceeb8c1e0a8c78c0a62a170194a5a3dc
Grosus <114068852+Grosus@users.noreply.github.com>
2022-10-19 11:05:04 +0200

    test

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def prepro(data: ClientData):
     df=preprocessing(data)
     with open('column.npy', 'rb') as f:
         cols=np.load(f,allow_pickle=True)
-    df=df[cols+'SK_ID_CURR']
+    df=df[cols,'SK_ID_CURR']
 
     return df.to_dict(orient='index')
     
